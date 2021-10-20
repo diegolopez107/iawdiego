2f9218c10191ef58f5dc10fece974d9a21ca2a5a
Diego Lopez Bnerbae <diego.lopez107@educa.madrid.org>
2021-10-20 14:01:18 +0200

    nuevo cambio

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="../js/prueba.js"></script>
-    <link href="../css/prueba.js" rel="stylesheet" type="text/css">
+    <link href="../css/prueba.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     
